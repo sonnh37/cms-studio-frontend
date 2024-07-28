99b570904b98847fc2057608531934745bb6674d
sonnh37 <nguyenson20zzzz@gmail.com>
2024-07-28 17:58:41 +0700

    fix script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -7,6 +7,7 @@ import { ThemeProvider } from "./provider";
 import { NavbarHeader } from "@/components/navbar";
 import { SessionProvider } from "next-auth/react";
 import { NextUIProvider } from "@nextui-org/react";
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -16,23 +17,23 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
       <head>
-      <script src="https://kit.fontawesome.com/b08cef85f4.js" crossOrigin="anonymous"></script>
+        {/* Use Script component for loading external scripts asynchronously */}
+        <Script src="https://kit.fontawesome.com/b08cef85f4.js" strategy="lazyOnload" crossOrigin="anonymous" />
       </head>
       <body>
-      <SessionProvider>
-            <NextUIProvider>
-              <ThemeProvider
-                attribute="class"
-                enableSystem={true}
-                defaultTheme="light"
-              >
-                <NavbarHeader />
-                {children}
-              </ThemeProvider>
-            </NextUIProvider>
-
-          </SessionProvider>
+        <SessionProvider>
+          <NextUIProvider>
+            <ThemeProvider
+              attribute="class"
+              enableSystem={true}
+              defaultTheme="light"
+            >
+              <NavbarHeader />
+              {children}
+            </ThemeProvider>
+          </NextUIProvider>
+        </SessionProvider>
       </body>
     </html>
-  )
+  );
 }
